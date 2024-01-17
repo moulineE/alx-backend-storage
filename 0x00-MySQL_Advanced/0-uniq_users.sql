9f8b6b41a7176ad2a812aef9004bc3003353e83f
moulineE <elmahdi.mouline@live.fr>
2024-01-17 14:44:51 +0000

    Task 0. We are all unique! --fix attr

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- a SQL script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
-	`id`INT,
-	`email` VARCHAR(255),
+	`id`INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`email` VARCHAR(255) NOT NULL UNIQUE,
 	`name`VARCHAR(255));
