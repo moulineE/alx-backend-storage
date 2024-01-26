c46ba81b4d303d6269adb2f4624f5b6e32bcb0ce
moulineE <elmahdi.mouline@live.fr>
2024-01-26 22:41:59 +0100

    Task 0. 1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """the Redis basic exercise module"""
 import redis
-from typing import Union
+from typing import Union, Optional, Callable, Any
 import uuid
 
 
@@ -21,3 +21,32 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """
+        method that take a key string argument and
+        an optional Callable argument named fn.
+        This callable will be used to convert the data
+        back to the desired format.
+        Args:
+            key(str): a key to the redis database data
+            fn(Callable): optional callable to convert the data
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return
+        if fn is str:
+            self.get_str(data)
+        if fn is int:
+            self.get_int(data)
+        if callable(fn):
+            return fn(data)
+        return data
+
+    def get_str(self, data: Any) -> str:
+        """convert data to string type"""
+        return data.decode("utf-8")
+
+    def get_int(self, data: Any) -> int:
+        """convert data to int type"""
+        return int(data)
